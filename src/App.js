e224a903f351fa3f2c5f780e252b5c632f902d6e
wstan2 <walter.scott.tan@gmail.com>
2020-01-23 12:44:48 -0600

    add yelp fusion api call to pull restaurant data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      rating: 0.0,
+      address: "",
+      city: "",
+      state: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/WavvLdfdP6g8aZTtbBQHTw`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_API_YELP_KEY}`
+          }
+        }
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({
+          name: res.data.name,
+          rating: res.data.rating,
+          address: res.data.location.address1,
+          city: res.data.location.city,
+          state: res.data.location.state
+        });
+      })
+
+      .catch(err => {
+        console.log("error");
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <span>{this.state.rating}</span>
+        <span>{this.state.address}</span>
+        <span>{this.state.city}</span>
+        <span>{this.state.state}</span>
+      </div>
+    );
+  }
 }
 
 export default App;
